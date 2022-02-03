b7e1a26203e6a44f314c60283470c0000b9f4a5f
va9iff <va9iff@gmail.com>
2022-02-04 03:53:33 +0400

    prototype of structure and windowing
    template of window is getting ready.
    desktop, start bar and stuff like this is getting ready too.

---
+++ src/apps/example/app.js
@@ -0,0 +1,9 @@
+export default {
+    url: "./index.html", //we don't have to have the real page in our repo.
+    // we can just to them with urls. here is on the repo tho.
+
+    onOpen: function(window){},
+    onOpenWith: function(window, files){},
+    
+    // icon: "./img/icon"
+}
