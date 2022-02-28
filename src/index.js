ea2de74720e535791417281638b4d761b5d07056
va9iff <va9iff@gmail.com>
2022-02-28 18:16:42 +0400

    fix scroll for overflown start bar app icons;
    with centered flex on flex hack
    that we used for previews
    also style changes
    transition duration and bg for app preveiws

--- src/index.js
+++ src/index.js
@@ -18,6 +18,35 @@ async function starter() {
 	// dynamically loading new apps, BUT they consume namespace in APPS
 	loadApp(a)
 	new Window(a).makeWindowElement(70,90)
+	 a = {
+		name: 'anonymweous'
+	}
+	// dynamically loading new apps, BUT they consume namespace in APPS
+	loadApp(a)
+	new Window(a).makeWindowElement(70,90)
+	 a = {
+		name: 'anonymofus'
+	}
+	// dynamically loading new apps, BUT they consume namespace in APPS
+	loadApp(a)
+	new Window(a).makeWindowElement(70,90)
+	 a = {
+		name: 'anongymous'
+	}
+	// dynamically loading new apps, BUT they consume namespace in APPS
+	loadApp(a)
+	new Window(a).makeWindowElement(70,90)
+	 a = {
+		name: 'anongymobfdsus'
+	}
+	loadApp(a)
+	new Window(a).makeWindowElement(70,90)
+	 a = {
+		name: 'anokmbluingymobfdsus'
+	}
+	// dynamically loading new apps, BUT they consume namespace in APPS
+	loadApp(a)
+	new Window(a).makeWindowElement(70,90)
 	new Window(APPS["example2"]).makeWindowElement(90,70)
 	let w = new Window(APPS["example2"])
 	w.makeWindowElement(50, 60)
