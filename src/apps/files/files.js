63dc6ef36f8b360b16c47b114b121206ab41d45d
va9iff <va9iff@gmail.com>
2022-03-12 00:09:40 +0400

    complete files explorer back and forth folders. problem on unescaping files

--- src/apps/files/files.js
+++ src/apps/files/files.js
@@ -1,79 +1,63 @@
 const BASE = "http://127.0.0.1:8080/"
 
-const browseWrapper = (path)=>{
-    window.parent.postMessage({
-        fun: "browse",
-        arg: path
-
-    }, "*")
-}
-
-browseWrapper('>')
-
-// console.log(window.parent.postMessage("WON", "*"))
-
-
-
-// => json
-async function browseAt(path) {
-    let url = BASE + path
-    let data = await fetch(url)
-    let folder = await data.json()
-    return folder
-    // downloadUsingAnchorElement(folder.directory[2])
-}
-
-// browseAt("src/apps/files/fakedirGET.json")
-
 var files_container = document.querySelector("#files")
+var currentPath = []
 
-function downloadUsingAnchorElement(fileName,url) {
-    let path = BASE + url
-    console.log(path)
-    anchor = document.createElement("a")
+
+function downloadUsingAnchorElement(fileName, url) {
+	let path = BASE + url
+	console.log(path)
+	anchor = document.createElement("a")
+    // !!! we need improvements on unescaping
+	// anchor.href = path.replace("%3E", ">")
     anchor.href = path
-    anchor.download = fileName
+	anchor.download = fileName
 
-    document.body.appendChild(anchor)
-    anchor.click()
-    anchor.remove()
+	document.body.appendChild(anchor)
+	anchor.click()
+	anchor.remove()
 }
 
-var downloadFile = fileName => {
-    let path = BASE + fileName
-    console.log(BASE, fileName)
-    let result = fetch(path)
-    result
-        .then(res => {
-            return res.text()
-        })
-        .then(res => {})
+async function refresh() {
+    path = ">"+currentPath.join(">")
+	let response = await browse(path)
+    console.log(response)
+	let folder = response.data
+
+	files_container.innerHTML = ""
+	for (fileName of folder.directory) {
+		let fileElement = document.createElement("div")
+		fileElement.className = "file"
+		fileElement.innerHTML = fileName
+		fileElement.onclick = fileClick
+		files_container.appendChild(fileElement)
+	}
 }
 
-async function buildFilesView(path){
-    let folder = await browseAt(path)
-    for (fileName of folder.directory){
-        let fileElement = document.createElement("div")
-        fileElement.className = "file"
-        fileElement.innerHTML = fileName
-        fileElement.onclick = function () {
-            console.log(folder.path)
-            downloadUsingAnchorElement(this.innerHTML, folder.path + this.innerHTML)
-        }
-        files_container.appendChild(fileElement)
-    }
+function fileClick() {
+	let path = unescape(this.innerHTML)
+	currentPath.push(path)
+	refresh()
 }
 
-buildFilesView('src/apps/files/fakedirGET.json')
+function back(){
+    currentPath.pop()
+    refresh()
+}
+
+document.querySelector(".back").onclick = back
+
+
+refresh("")
 
 // for (file of directory) {
-    // let fileElement = document.createElement("div")
-    // fileElement.className = "file"
-    // fileElement.innerHTML = file.name
-    // fileElement.onclick = function () {
-        // console.log(this)
-        // downloadUsingAnchorElement(this.innerHTML)
-    // }
-    // files_container.appendChild(fileElement)
+// let fileElement = document.createElement("div")
+// fileElement.className = "file"
+// fileElement.innerHTML = file.name
+// fileElement.onclick = function () {
+// console.log(this)
+// downloadUsingAnchorElement(this.innerHTML)
 // }
-// 
+// files_container.appendChild(fileElement)
+// }
+//
