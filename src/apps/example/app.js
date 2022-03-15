8454ae5ba1018e2c0aead862b546aecab9ddd492
va9iff <va9iff@gmail.com>
2022-03-15 21:41:05 +0400

    also using $WON in app.js for refering current url

--- src/apps/example/app.js
+++ src/apps/example/app.js
@@ -2,6 +2,7 @@ export default {
     name: "example",
     startX: 100,
     startY: 100,
+    // $WON means the current url that WON is opened
     home: "$WON/src/apps/example/index.html", //we don't have to have the real page in our repo.
     icon: "$WON/src/apps/example/example_icon.png",
     // we can just to them with urls. here is on the repo tho.
