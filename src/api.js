673387424fb7ae2f68ffb0719ba25a234c173583
va9iff <va9iff@gmail.com>
2022-03-23 02:16:15 +0400

    working github browsing

--- src/api.js
+++ src/api.js
@@ -2,6 +2,11 @@
 // const WONU = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
 const WONU = window.location.href
 
+const repo = "Portfolio"
+const user = "va9iff"
+const url  = `https://api.github.com/repos/${user}/${repo}/contents/`
+
+
 async function browse(path="tmp") {
   try {
     const response = await fetch(`http://localhost:5000/browse/${path}`);
@@ -20,7 +25,7 @@ async function browse(path="tmp") {
   }
 }
 
-async function save(file, name, folder="tmp") {
+async function save(file, name, folder="") {
   const formData = new FormData()
   formData.append("file", file)
   formData.append("name", name)
@@ -37,3 +42,26 @@ async function save(file, name, folder="tmp") {
   }
 }
 
+async function browseOnGitHub(path = "") {
+  path = path.replace('>',"/")
+  let response = await fetch(`${url}${path}`)
+  let k = await response.clone()
+  k = await k.json()
+  // make directory with k[n].name
+  if (k.download_url) {
+    k = await fetch(k.download_url)
+    k = await k.blob()
+    return k
+  }
+  k.directory = k.map(p=>p.name)
+  return k
+}
+
+
+async function LTbrowseOnGitHub(path = ""){
+  path = path.replace(">","/")
+  let res = await browseOnGitHub(path)
+  return res
+}
+
+browse = browseOnGitHub
